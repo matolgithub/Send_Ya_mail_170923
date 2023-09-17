3fe94f6fdeb2aa5a1aea747f82a8ce1ab701c03a
Oleg <89175955650@mail.ru>
2023-09-17 19:00:03 +0300

    Next commit.

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+def send_ya_mail(recipients_emails: list, msg_text: str):
+    pass
+
+
+def main():
+    send_ya_mail()
+
+
+if __name__ == "__main__":
+    main()
