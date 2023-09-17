944796c05acda117f051dfa2c3bd8ca26b7b87f3
Oleg <89175955650@mail.ru>
2023-09-17 21:04:33 +0300

    Finish commit.

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ from email.header import Header
 
 
 def send_ya_mail(recipients_emails: list, msg_text: str):
-    pass
+    login = "&&&&&&&@yandex.ru"
+    password = "*(^%$##9gsve"
+
+    msg = MIMEText(f"{msg_text}", "plain", "utf-8")
+    msg["Subject"] = Header("Важно!", "utf-8")
+    msg["From"] = login
+    msg["To"] = ", ".join(recipients_emails)
+
+    s = smtplib.SMTP("smtp.yandex.ru", 587, timeout=10)
+
+    try:
+        s.starttls()
+        s.login(login, password)
+        s.sendmail(msg["From"], recipients_emails, msg.as_string())
+    except Exception as ex:
+        print(ex)
+    finally:
+        s.quit()
 
 
 def main():
-    send_ya_mail()
+    send_ya_mail(["******@mail.ru", "^^^^^^@gmail.com"], "Hi, it is great!")
 
 
 if __name__ == "__main__":
